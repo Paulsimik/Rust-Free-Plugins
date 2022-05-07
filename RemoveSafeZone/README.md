7d7c3cde92e26b5c9973cdfad2dbc9525bc6cae0
ƤᗩᑌŁ <pavelsimik@gmail.com>
2022-05-08 00:53:15 +0200

    Plugin release

---
+++ RemoveSafeZone/README.md
@@ -0,0 +1,2 @@
+# Remove Safe Zone
+    Removing all safe zones on map. No permissions, no config, just install.
---
+++ RemoveSafeZone/RemoveSafeZone.cs
@@ -0,0 +1,33 @@
+﻿/*
+ ########### README ####################################################
+                                                                             
+  !!! DON'T EDIT THIS FILE !!!
+                                                                     
+ ########### CHANGES ###################################################
+
+ 1.0.0
+    - Plugin release
+
+ #######################################################################
+*/
+
+namespace Oxide.Plugins
+{
+    [Info("Remove Safe Zone", "Paulsimik", "1.0.0")]
+    [Description("Removing all safe zones on map")]
+    class RemoveSafeZone : RustPlugin
+    {
+        private void OnServerInitialized()
+        {
+            var obj = UnityEngine.Object.FindObjectsOfType<TriggerSafeZone>();
+            if (obj != null)
+            {
+                foreach (var objects in obj)
+                {
+                    if (objects != null)
+                        UnityEngine.Object.Destroy(objects);
+                }
+            }
+        }
+    }
+}
